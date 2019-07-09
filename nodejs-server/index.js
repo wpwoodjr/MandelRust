bcfe8ed49f1cbe1b9d3cdb74c0fd013ac3336dff
Bill Wood <william.wood@gsk.com>
2019-07-09 16:58:13 -0400

    tweaks to get it working with node

--- nodejs-server/index.js
+++ nodejs-server/index.js
@@ -7,7 +7,7 @@ var fs = require('fs'),
 var app = require('connect')();
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
-var serverPort = 8080;
+var serverPort = 8081;
 
 // swaggerRouter configuration
 var options = {
