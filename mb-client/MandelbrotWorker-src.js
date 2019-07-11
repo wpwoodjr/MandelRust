ff9f1152bf2dc84e77714d1973727c2bf04b0518
Bill Wood <wpwoodjr@gmail.com>
2019-07-10 22:11:12 -0400

    use fetch

--- mb-client/MandelbrotWorker-src.js
+++ mb-client/MandelbrotWorker-src.js
@@ -1,42 +1,28 @@
-var Mandelbrot = require('mandelbrot');
 
-var api = new Mandelbrot.MandelbrotApi()
-
-var mandelbrotCoords = new Mandelbrot.MandelbrotCoords(); // {MandelbrotCoords} Mandelbrot coordinates to compute
-
-/*
-api.computeMandelbrot(mandelbrotCoords).then(function(data) {
-  console.log('API called successfully. Returned data: ' + data);
-}, function(error) {
-  console.error(error);
-});
-*/
+async function fetchIterationCounts(coords) {
+    let response = await fetch("url", {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, cors, *same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'omit', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json',
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrer: 'no-referrer', // no-referrer, *client
+        body: JSON.stringify(coords), // body data type must match "Content-Type" header
+    });
+    return await response.json();
+}
 
 function compute(y, xmin, dx, columns, maxIterations) {
 //    console.log(y + ", " + xmin + ", " + dx + ", " + columns + ", " + maxIterations);
 // -1.1899791231732677, -2.2, 0.005008347245409015, 600, 100
-    console.log(mandelbrotCoords);
-    iterationCounts = [];
-    let x0 = xmin;
-    for (let i = 0; i < columns; i++) {
-        let y0 = y;
-        let a = x0;
-        let b = y0;
-        let ct = 0;
-        while (a*a + b*b < 4.1) {
-            ct++;
-            if (ct > maxIterations) {
-                ct = -1;
-                break;
-            }
-            let newa = a*a - b*b + x0;
-            b = 2*a*b + y0;
-            a = newa;
-        }
-        iterationCounts[i] = ct;
-        x0 += dx;
-    }
-    return iterationCounts;
+
+    return fetchIterationCounts({
+        y: y, xmin: xmin, dx: dx, columns: columns, maxIterations: maxIterations
+    })
 }
 
 onmessage = function(msg) {
