909f954ab51c7bfb2d1ad98c2ca86fbf6e5b7abe
Bill Wood <wpwoodjr@gmail.com>
2023-02-11 16:57:20 -0500

    add wasm target during build

--- mb-kubernetes/mb-wasm/build.sh
+++ mb-kubernetes/mb-wasm/build.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+# build Mandelbrot wasm code using Rust
+
+rustup target add wasm32-unknown-unknown
 cargo build --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/mb_wasm.wasm ../client/mb-wasm.wasm
