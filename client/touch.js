c8ff7289213e2b97b99e91c4f4b756a38be08861
Bill Wood <wpwoodjr@gmail.com>
2023-03-22 19:05:00 -0400

    revert to older version of touch.js

--- client/touch.js
+++ client/touch.js
@@ -6,10 +6,9 @@
 class Touch {
     constructor(id, options) {
         this.id = id;
-        this.element = document.getElementById(id);
+        this.parentElement = document.getElementById(id);
         options = options || {};
         this.onInit = options.onInit || null;
-        this.onTouchStart = options.onTouchStart || null;
         this.onDragStart = options.onDragStart || null;
         this.onDragMove = options.onDragMove || null;
         this.onDragEnd = options.onDragEnd || null;
@@ -18,51 +17,35 @@ class Touch {
         this.onPinchStart = options.onPinchStart || null;
         this.onPinchMove = options.onPinchMove || null;
         this.onPinchEnd = options.onPinchEnd || null;
-        this.allowDocumentTouches = options.allowDocumentTouches || false;
         this.data = options.data || {};
 
         // Variables to store touch positions and state
+        this.init = false;
         this.startTouches = [];
         this.isDragging = false;
+        this.dragId = NaN;
         this.isPinching = false;
         this.isTapping = false;
         this.singleTapTimeout = null;
-        this.element.addEventListener("touchstart", (event) => this.handleTouchStart(event), {passive: false});
-        this.element.addEventListener("touchmove", (event) => this.handleTouchMove(event), {passive: false});
-        this.element.addEventListener("touchend", (event) => this.handleTouchEnd(event), {passive: false});
-        this.element.addEventListener("touchcancel", (event) => this.handleTouchCancel(event), {passive: false});
-        if (! this.allowDocumentTouches) {
-            // document.addEventListener('touchstart', (event) => this.handleDocumentTouchEvent(event), {passive: false});
-            // document.addEventListener('touchmove', (event) => this.handleDocumentTouchEvent(event), {passive: false});
-            // document.addEventListener('touchend', (event) => this.handleDocumentTouchEvent(event), {passive: false});
-        }
-    }
-
-    // prevent touches on the document from interfering with element touches
-    handleDocumentTouchEvent(event) {
-        if (this.startTouches.length !== 0) {
-            event.preventDefault();
-        }
+        this.parentElement.addEventListener("touchstart", (event) => this.handleTouchStart(event), {passive: false});
+        this.parentElement.addEventListener("touchmove", (event) => this.handleTouchMove(event), {passive: false});
+        this.parentElement.addEventListener("touchend", (event) => this.handleTouchEnd(event), {passive: false});
+        this.parentElement.addEventListener("touchcancel", (event) => this.handleTouchCancel(event), {passive: false});
     }
 
     handleTouchStart(event) {
         // console.log("touch start");
-        if (this.onInit) {
-            this.onInit();
-            this.onInit = null;
+        if (! this.init) {
+            this.init = true;
+            if (this.onInit) {
+                this.onInit();
+            }
+        } else if (this.isDragging || this.isPinching || this.isTapping) {
+            return;
         }
 
-        if (this.onTouchStart) {
-            this.onTouchStart();
-        }
-
-        // Store the touch positions if touch events aren't happening outside the target element
-        // and dragging, pinching, or tapping have not started yet
-        if (event.touches.length === event.targetTouches.length
-            && ! (this.isDragging || this.isPinching || this.isTapping)) {
-        // if (! (this.isDragging || this.isPinching || this.isTapping)) {
-            this.startTouches = this.copyTouches(event.targetTouches);
-        }
+        // Store the initial touch positions
+        this.startTouches = this.copyTouches(event.targetTouches);
     }
       
     // Handle touch move event
@@ -71,29 +54,28 @@ class Touch {
 
         // check for continue drag
         if (this.isDragging) {
-            event.preventDefault();
             if (this.onDragMove) {
-                const id = this.startTouches[0].identifier;
                 for (const e of event.changedTouches) {
-                    if (e.identifier === id) {
+                    if (e.identifier === this.dragId) {
                         this.onDragMove(e.clientX, e.clientY);
                         break;
                     }
                 }
             }
 
-        // check for continue two finger pinching///???check ids?
+        // check for continue two finger pinching
         } else if (this.isPinching) {
-            event.preventDefault();
             if (this.onPinchMove) {
-                this.onPinchMove(event.targetTouches[0].clientX, event.targetTouches[0].clientY,
-                    event.targetTouches[1].clientX, event.targetTouches[1].clientY);
+                if (event.targetTouches.length === 2) {
+                    this.onPinchMove(event.targetTouches[0].clientX, event.targetTouches[0].clientY,
+                        event.targetTouches[1].clientX, event.targetTouches[1].clientY);
+                }
             }
 
         // check for start of one touch drag
         } else if (event.targetTouches.length === 1 && this.startTouches.length === 1) {
-            event.preventDefault();
             this.isDragging = true;
+            this.dragId = this.startTouches[0].identifier;
             if (this.onDragStart) {
                 this.onDragStart(this.startTouches[0].clientX, this.startTouches[0].clientY);
             };
@@ -103,7 +85,6 @@ class Touch {
 
         // check if there are two touches for pinch gesture
         } else if (event.targetTouches.length === 2 && this.startTouches.length === 2) {
-            event.preventDefault();
             this.isPinching = true;
             if (this.onPinchStart) {
                 this.onPinchStart(this.startTouches[0].clientX, this.startTouches[0].clientY,
@@ -113,6 +94,11 @@ class Touch {
                 this.onPinchMove(event.targetTouches[0].clientX, event.targetTouches[0].clientY,
                     event.targetTouches[1].clientX, event.targetTouches[1].clientY);
             }
+        }
+
+        if (this.isDragging || this.isPinching) {
+            // Prevent scrolling on the page
+            event.preventDefault();
         }
     }
 
@@ -124,7 +110,12 @@ class Touch {
             this.dragEnd(event);
 
         } else if (this.isPinching) {
-            this.pinchEnd(event);
+            if (event.touches.length === 0) {
+                this.isPinching = false;
+                if (this.onPinchEnd) {
+                    this.onPinchEnd();
+                }
+            }
 
         // Check for tap gesture
         } else if (event.targetTouches.length === 0 && this.startTouches.length === 1) {
@@ -133,7 +124,9 @@ class Touch {
 
             // If the single tap setTimeout hasn't fired, it's a double tap gesture
             if (this.singleTapTimeout != null) {
-                this.tapEnd();
+                this.isTapping = false;
+                clearTimeout(this.singleTapTimeout);
+                this.singleTapTimeout = null;
                 if (this.onDoubleTap) {
                     // prevent emulated mouse dblclick
                     event.preventDefault();
@@ -148,14 +141,13 @@ class Touch {
             else {
                 this.isTapping = true;
                 this.singleTapTimeout = setTimeout(() => {
-                    this.tapEnd();
+                    this.isTapping = false;
+                    this.singleTapTimeout = null;
                     if (this.onSingleTap) {
                         this.onSingleTap(startX, startY);
                     }
                 }, 350);
             }
-        } else {
-            this.startTouches = [];
         }
     }
  
@@ -165,48 +157,29 @@ class Touch {
         if (this.isDragging) {
             this.dragEnd(event);
         } else if (this.isPinching) {
-            this.pinchEnd(event);
+            this.isPinching = false;
+            if (this.onPinchEnd) {
+                this.onPinchEnd();
+            }
         } else if (this.isTapping) {
-            this.tapEnd();
+            this.isTapping = false;
+            if (this.singleTapTimeout) {
+                clearTimeout(this.singleTapTimeout);
+                this.singleTapTimeout = null;
+            }
         }
     }
 
     dragEnd(event) {
-        const id = this.startTouches[0].identifier;
         for (const e of event.changedTouches) {
-            if (e.identifier === id) {
+            if (e.identifier === this.dragId) {
                 this.isDragging = false;
-                this.startTouches = [];
+                this.dragId = NaN;
                 if (this.onDragEnd) {
                     this.onDragEnd();
                 }
                 return;
             }
-        }
-    }
-
-    pinchEnd(event) {
-        const id0 = this.startTouches[0].identifier;
-        const id1 = this.startTouches[1].identifier;
-        for (const e of event.changedTouches) {
-            // end if one of the original fingers was lifted
-            if (e.identifier === id0 || e.identifier === id1) {
-                this.isPinching = false;
-                this.startTouches = [];
-                if (this.onPinchEnd) {
-                    this.onPinchEnd();
-                }
-                return;
-            }
-        }
-    }
-
-    tapEnd() {
-        this.isTapping = false;
-        this.startTouches = [];
-        if (this.singleTapTimeout) {
-            clearTimeout(this.singleTapTimeout);
-            this.singleTapTimeout = null;
         }
     }
 
